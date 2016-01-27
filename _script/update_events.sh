2f49a3fe356a7924058fac5dfecdb35aaaebf047
Ross Penman <ross@pen.mn>
2016-01-27 03:06:45 +0000

    Use YAML extension for events
    Jekyll JSON parsing is actually YAML parsing,
    so we might as well be honest about it.
    
    This causes problems for JSON-escaped slashes.
    
    Why are slashes even escaped in JSON?

--- _script/update_events.sh
+++ _script/update_events.sh
@@ -3,4 +3,4 @@ set -ue
 
 source .env
 mkdir -p _data
-curl "https://graph.facebook.com/$GROUP_ID/events?since=$(date +%s)&access_token=$ACCESS_TOKEN" | sed 's/\\\//\//g' > _data/events.json
+curl "https://graph.facebook.com/$GROUP_ID/events?since=$(date +%s)&access_token=$ACCESS_TOKEN" | sed 's/\\\//\//g' > _data/events.yml
