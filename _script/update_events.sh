41bba5c72bc3fc9b3c2d9323f101993e14a47fdc
Ross Penman <ross@pen.mn>
2016-01-27 23:41:02 +0000

    Download images

--- _script/update_events.sh
+++ _script/update_events.sh
@@ -2,8 +2,20 @@
 set -ue
 
 source .env
-url="https://graph.facebook.com/$GROUP_ID/events?since=$(date +%s)&access_token=$ACCESS_TOKEN"
+api="https://graph.facebook.com/v2.5"
+auth="access_token=$ACCESS_TOKEN"
+url="$api/$GROUP_ID/events?since=$(date +%s)&$auth"
+echo "$url"
 
 mkdir -p _data
+json="$(curl "$url")"
 echo --- > _data/events.yml
-curl "$url" | sed 's/\\\//\//g' >> _data/events.yml
+printf '%s' "$json" | sed 's/\\\//\//g' >> _data/events.yml
+
+# Download images
+mkdir -p images/events
+for id in $(printf '%s' "$json" | jq -r '.data[].id'); do
+  url="$(printf '%s\n' "$api/$id/photos?$auth")"
+  photo_id="$(curl -L "$url" | jq -r '.data[0].id')"
+  curl -L "$api/$photo_id/picture?$auth" -o "images/events/$id.jpeg"
+done
