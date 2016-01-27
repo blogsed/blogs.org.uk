b35b80275086b4ef793ff11bf472432bc82aa9ec
Ross Penman <ross@pen.mn>
2016-01-27 02:03:59 +0000

    Better release script

---
+++ _scripts/release.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -ue
+
+echo "\033[1mStashing changes\033[0m"
+git stash
+branch="$(git name-rev --name-only HEAD)"
+git checkout --orphan release
+echo "\033[1mBuilding site\033[0m"
+jekyll build
+git reset
+git add -f _site
+git commit -m "Generate site"
+echo "\033[1mPushing to GitHub\033[0m"
+git push origin `git subtree split --prefix _site HEAD`:refs/heads/release --force
+git add -A
+git checkout "$branch"
+git branch -D release
+echo "\033[1mRestoring changes\033[0m"
+git stash pop > /dev/null
