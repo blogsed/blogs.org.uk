1b21380889d3fbf62d67116af6aa7900462b8db2
Ross Penman <ross@pen.mn>
2016-01-27 03:10:07 +0000

    Add YAML front matter

--- _script/update_events.sh
+++ _script/update_events.sh
@@ -3,4 +3,5 @@ set -ue
 
 source .env
 mkdir -p _data
-curl "https://graph.facebook.com/$GROUP_ID/events?since=$(date +%s)&access_token=$ACCESS_TOKEN" | sed 's/\\\//\//g' > _data/events.yml
+echo "---" > _data/events.yml
+curl "https://graph.facebook.com/$GROUP_ID/events?since=$(date +%s)&access_token=$ACCESS_TOKEN" | sed 's/\\\//\//g' >> _data/events.yml
