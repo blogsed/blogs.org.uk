c6bbb320c82768692b1b900d456d0fccbeaaf53b
Ross Penman <ross@pen.mn>
2016-01-27 23:58:24 +0000

    Release to master

--- _script/release.sh
+++ _script/release.sh
@@ -4,16 +4,16 @@ set -ue
 echo "\033[1mStashing changes\033[0m"
 git stash
 branch="$(git name-rev --name-only HEAD)"
-git checkout --orphan release
+git checkout --orphan master
 echo "\033[1mBuilding site\033[0m"
 bundle exec jekyll build
 git reset
 git add -f _site
 git commit -m "Generate site"
 echo "\033[1mPushing to GitHub\033[0m"
-git push origin `git subtree split --prefix _site HEAD`:refs/heads/release --force
+git push origin `git subtree split --prefix _site HEAD`:refs/heads/master --force
 git add -A
 git checkout "$branch"
-git branch -D release
+git branch -D master
 echo "\033[1mRestoring changes\033[0m"
 git stash pop > /dev/null
