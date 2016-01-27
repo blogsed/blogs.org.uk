c05842ba84852805d17c827b8fbae2bcc9a63116
Ross Penman <ross@pen.mn>
2016-01-27 03:23:09 +0000

    Reduce line length

--- _script/update_events.sh
+++ _script/update_events.sh
@@ -2,6 +2,8 @@
 set -ue
 
 source .env
+url="https://graph.facebook.com/$GROUP_ID/events?since=$(date +%s)&access_token=$ACCESS_TOKEN"
+
 mkdir -p _data
-echo "---" > _data/events.yml
-curl "https://graph.facebook.com/$GROUP_ID/events?since=$(date +%s)&access_token=$ACCESS_TOKEN" | sed 's/\\\//\//g' >> _data/events.yml
+echo --- > _data/events.yml
+curl "$url" | sed 's/\\\//\//g' >> _data/events.yml
