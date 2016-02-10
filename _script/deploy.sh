453ba4e0df24708a8ef9153959acd7c36f8c06d5
Ross Penman <ross@pen.mn>
2016-02-10 18:36:27 +0000

    Update deploy script

--- _script/deploy.sh
+++ _script/deploy.sh
@@ -7,7 +7,7 @@ git stash
 branch="$(git name-rev --name-only HEAD)"
 echo "\033[1mPushing to GitHub\033[0m"
 git checkout -b master
-git add -f _data/events.yml images/events
+git add -f _data/events/group.yml _data/events/page.yml images/events
 git commit -m "Include auto-generated content"
 git push -f origin master
 git checkout "$branch"
