5cb0e361742a8955eb90244790432d20e4178189
Ross Penman <ross@pen.mn>
2016-03-24 12:46:43 +0000

    Better output for _script/deploy.sh

--- _script/deploy.sh
+++ _script/deploy.sh
@@ -4,7 +4,9 @@ set -ue
 echo "\033[1mStashing changes\033[0m"
 git add -A
 set +e
-[ "$(git stash)" != "No local changes to save" ]
+stash_message="$(git stash)"
+echo "$stash_message"
+[ "$stash_message" != "No local changes to save" ]
 stashed=$?
 set -e
 branch="$(git name-rev --name-only HEAD)"
@@ -15,7 +17,7 @@ git commit -m "Include auto-generated content"
 git push -f origin master
 git checkout "$branch"
 git branch -D master
-echo "\033[1mRestoring changes\033[0m"
 if [ "$stashed" = "0" ]; then
+  echo "\033[1mRestoring changes\033[0m"
   git stash pop > /dev/null
 fi
